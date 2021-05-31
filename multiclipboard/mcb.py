cca5bffb1561eb55ebfa1c6853a0ffae2e4ed1ce
Rohit Nikam <rohitnikam882@gmail.com>
2021-05-31 21:28:55 +0200

    added delete mothods

--- multiclipboard/mcb.py
+++ multiclipboard/mcb.py
@@ -2,17 +2,23 @@
 
 # Saves and loads  multiple pieces of text to the clipboard
 #Usage:
-# mcb.pyw save <keyword> (saves clipboard to keyword)
-# mcb.pyw <keyword> (loads keyword to clipboard)
-# mcb.pyw list (loads all keywords to clipboard)
+# python mcb.py save <keyword>    (saves clipboard to keyword)
+# python mcb.py delete <keyword>  (deletes the keyword entry)
+# python mcb.py <keyword>         (loads keyword to clipboard)
+# python mcb.py list              (loads all keywords to clipboard)
+# python mcb.py deleteall         (deletes all entries)
 
 import shelve, pyperclip, sys
 
 mcbshelf = shelve.open('mcb')
 
 # save clipboard content
-if len(sys.argv) == 3 and sys.argv[1].lower()=='save':
-    mcbshelf[sys.argv[2]] = pyperclip.paste()
+if len(sys.argv) == 3:
+    if sys.argv[1].lower()=='save':
+        mcbshelf[sys.argv[2]] = pyperclip.paste()
+    elif sys.argv[1].lower()=='delete':
+        mcbshelf.pop(sys.argv[2])
+        pyperclip.copy(str(list(mcbshelf.items())))
 
 # list keywords
 elif sys.argv[1]=='list':
@@ -21,5 +27,10 @@ elif sys.argv[1]=='list':
 # load content
 elif sys.argv[1] in mcbshelf:
     pyperclip.copy(mcbshelf[sys.argv[1]])
+
+# delete all entries
+elif sys.argv[1]=='deleteall':
+    mcbshelf.clear()
+    pyperclip.copy(str(list(mcbshelf.items())))
 
 mcbshelf.close()
